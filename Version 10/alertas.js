8dacd6fcfd2e4f33dcda697d56f2a64490cdb182
Carlos-Guirola <158133600+Carlos-Guirola@users.noreply.github.com>
2024-05-30 11:45:49 -0600

    Delete Version 10 directory

--- Version 10/alertas.js
+++
@@ -1,9 +0,0 @@
-
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-
-// Función para cerrar el modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
